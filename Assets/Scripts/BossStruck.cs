4b79d5de90b48e6679f057e2a5d0457e481b3ac1
HeyImRige <Robert.aron.tx@verizon.net>
2018-04-15 18:05:51 -0500

    strike animation basicc

--- Assets/Scripts/BossStruck.cs
+++ Assets/Scripts/BossStruck.cs
@@ -4,33 +4,58 @@ using UnityEngine;
 
 public class BossStruck : MonoBehaviour {
 
-	/// <summary>
-	/// OnTriggerEnter is called when the Collider other enters the trigger.
-	/// </summary>
-	/// <param name="other">The other Collider involved in this collision.</param>
 
-	public Deform.Deformers.CurveDeformer bossDeform;
-	public GameObject curveAxis;
-	private Transform curveTransform;
-	/// <summary>
-	/// Start is called on the frame when a script is enabled just before
-	/// any of the Update methods is called the first time.
-	/// </summary>
+	private Deform.Deformers.CurveDeformer bossDeform;
+	public GameObject deformedAxis;
+	private Transform deformedTransformAxis;
+	private float hitDampen = 10;
+	public float strikeTime = 0.1f;
+	public float bounceTime = 0.1f;
 	void Start()
 	{
 		bossDeform = GetComponent<Deform.Deformers.CurveDeformer>();
-		curveTransform = curveAxis.GetComponent<Transform>();
+		deformedTransformAxis = deformedAxis.GetComponent<Transform>();
 	}
 
 	void OnTriggerEnter(Collider other)
 	{
 		if(other.CompareTag("Player")){
-			Debug.Log("WHOO!!");
 			Rigidbody otherRigid = other.GetComponent<Rigidbody>();
 			float hitStrength = otherRigid.velocity.magnitude;
 			Vector3 hitDirection = otherRigid.velocity.normalized;
-			bossDeform.strength = hitStrength/20;
-			curveTransform.rotation = Quaternion.Euler(Mathf.Atan2(hitDirection.y*-1,hitDirection.x)*Mathf.Rad2Deg-90,90,0);
+			// TODO: find hit location so the deform isn't always centered.
+			StartCoroutine(stretchBounceCoroutine(hitDirection,hitStrength,0.5f));
+		}
+	}
+
+
+	/// <summary>
+	/// Deforms the boss. Quickly moves to full deform, bounces back and past, then back to resing position.
+	/// </summary>
+	private IEnumerator stretchBounceCoroutine(Vector3 hitDirection, float hitStrength, float hitPosition){
+		float maxDeform = hitStrength/hitDampen;
+		float secondDeform = maxDeform*-0.3f;
+		deformedTransformAxis.rotation = Quaternion.Euler(Mathf.Atan2(hitDirection.y*-1,hitDirection.x)*Mathf.Rad2Deg-90,90,0);
+		// Strike
+		while(Mathf.Abs(maxDeform - bossDeform.strength) > Mathf.Epsilon)
+		{
+			Debug.Log(maxDeform-bossDeform.strength);
+			bossDeform.strength = Mathf.MoveTowards(bossDeform.strength,maxDeform,Time.deltaTime/strikeTime);
+			yield return null;
+		}
+		// Bounce
+		while(Mathf.Abs(secondDeform - bossDeform.strength) > Mathf.Epsilon)
+		{
+			Debug.Log("Bounce");
+			bossDeform.strength = Mathf.MoveTowards(bossDeform.strength,secondDeform,Time.deltaTime/bounceTime);
+			yield return null;
+		}
+		// Return
+		while(Mathf.Abs(0f - bossDeform.strength) > Mathf.Epsilon)
+		{
+			Debug.Log("Return");
+			bossDeform.strength = Mathf.MoveTowards(bossDeform.strength,0f,Time.deltaTime/bounceTime);
+			yield return null;
 		}
 	}
 }
