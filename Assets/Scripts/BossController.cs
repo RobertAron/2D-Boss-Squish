be03d9b26029fb3308150d58dab4437ea21569df
RobertAron <Robert.aron.tx@verizon.net>
2018-06-02 18:01:42 -0500

    updated to work on coroutine

--- Assets/Scripts/BossController.cs
+++ Assets/Scripts/BossController.cs
@@ -4,18 +4,33 @@ using UnityEngine;
 
 public class BossController : MonoBehaviour {
 	public GameObject defenseMove;
+	public int defenseOrbSpawnAmount = 1;
 	public float timeTillOrbSpawn = 5f;
 	private float totalTime = 0f;
-	private bool hasSpawned = false; 
+	private bool hasSpawned = false;
 	
 	// Update is called once per frame
-	void Update () {
+	// void Update () {
+	// 	totalTime+= Time.deltaTime;
+	// 	if(totalTime> timeTillOrbSpawn && !hasSpawned)
+	// 	{
+	// 		var defenseInstnace = Object.Instantiate(defenseMove,Vector3.zero,Quaternion.identity,transform).GetComponent<DefenseMove>();
+	// 		defenseInstnace.Initialize(3);
+	// 		hasSpawned = true;
+	// 	}
+	// }
+
+	private void Update() {
 		totalTime+= Time.deltaTime;
-		if(totalTime> timeTillOrbSpawn && !hasSpawned)
+		if(!hasSpawned && totalTime>5)
 		{
-			var defenseInstnace = Object.Instantiate(defenseMove,Vector3.zero,Quaternion.identity,transform).GetComponent<DefenseMove>();
-			defenseInstnace.Initialize(3);
+			ActivateDefenseMove();
 			hasSpawned = true;
-		}
+		} 
+	}
+
+	[ContextMenu("Defense Move")]
+	void ActivateDefenseMove(){
+		defenseMove.GetComponent<DefenseMoveController>().DefenseMove(defenseOrbSpawnAmount);
 	}
 }
---
+++ Assets/Scripts/DefenseMoveController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DefenseMoveController : MonoBehaviour {
+	public GameObject defenseOrbBase;
+	private GameObject[] orbs;
+	// Use this for initialization
+	void Start () {
+		orbs = new GameObject[10];
+		for(int i=0;i<10;i++)
+		{
+			orbs[i] = Object.Instantiate(defenseOrbBase,Vector3.zero,Quaternion.identity,transform);
+		}
+	}
+	
+	public void DefenseMove(int orbAmount)
+	{
+		float rotationOffset = 360f/orbAmount;
+		float currentRotation = 0f;
+		for(int i=0;i<orbAmount;i++)
+		{
+			currentRotation+=rotationOffset;
+			orbs[i].transform.rotation = Quaternion.Euler(0,0,currentRotation);
+			StartCoroutine(orbs[i].transform.GetChild(0).GetComponent<DefenseOrbMovement2>().Move());
+		}
+	}
+}
